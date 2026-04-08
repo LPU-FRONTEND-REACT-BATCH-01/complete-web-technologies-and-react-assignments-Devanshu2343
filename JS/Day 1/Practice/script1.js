ccc828db6c31f212d80809bf2c9b01d9dfa3fc36
Devanshu <devanshu2343@gmail.com>
2026-04-08 16:07:28 +0530

    let var const of JS

---
+++ JS/Day 1/Practice/script1.js
@@ -0,0 +1,22 @@
+var a ;
+let b ;
+
+console.log(a);
+ 
+console.log(b);
+// const c ; // const declaration must be initialized
+
+a =20 ;
+
+b =40 ;
+
+var a ;
+
+//let b ;  // can not redeclare , block level scope
+
+console.log(a);
+console.log(b);
+
+
+
+
---
+++ JS/Day 1/Practice/script3.js
@@ -0,0 +1,21 @@
+console.log(a);
+console.log(d);
+
+var b ;
+
+var a =20 ;
+
+console.log(b);
+
+let c =30 ;
+
+console.log(d); //-> refernce error
+
+let d ;
+
+console.log(d);
+
+
+
+
+
