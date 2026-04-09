0cc5b71b543cc96a76755f302369a03631783191
Devanshu <devanshu2343@gmail.com>
2026-04-09 12:37:50 +0530

    assigment

---
+++ JS/Day 2/Practice/scopeScript.js
@@ -0,0 +1,35 @@
+let a = 10 ;
+let b = 20 ;
+
+
+function test() {
+    var a = 30 ;
+    const d = 50 ;
+    let local_variable = 50 ;
+    console.log(a);
+    
+    
+}
+
+
+
+console.log(a);
+console.log(b);
+
+if (true){
+    let a = 10
+    const b =20
+    var c =40
+}
+
+function demo2() {
+    var demo =20 ;
+    
+}
+
+console.log(a);
+console.log(c);
+console.log(window.a);
+
+
+
