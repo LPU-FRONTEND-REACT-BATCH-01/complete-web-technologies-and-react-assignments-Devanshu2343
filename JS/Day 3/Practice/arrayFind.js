7d73c87b560d93cbb9ec00b3661abb31dc6d3e86
Devanshu <devanshu2343@gmail.com>
2026-04-10 11:49:41 +0530

    assigment done

---
+++ JS/Day 3/Practice/arrayFind.js
@@ -0,0 +1,35 @@
+let users = [
+    {
+        id: 1,
+        username: "devanshu1010",
+        password: "QWERTY"
+    },
+    {
+        id: 2,
+        username: "abhi",
+        password: "QWERTY1"
+    },
+    {
+        id: 3,
+        username: "sama",
+        password: "QWERTY22"
+    }
+];
+
+let inputData = {
+    userName: "devanshu1010",
+    password: "QWERTY1"
+};
+
+// Find user by username
+let user = users.find(ele => ele.username === inputData.userName);
+
+if (!user) {
+    console.log("Incorrect username ! Return to homepage");
+}
+else if (user.password !== inputData.password) {
+    console.log("Incorrect Password");
+}
+else {
+    console.log("Login Successful");
+}
